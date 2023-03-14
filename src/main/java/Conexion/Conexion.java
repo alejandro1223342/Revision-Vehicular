0af535bc79a3601a741bc5b6b9fa451a26f5047d
Alejoss <alejomuoss@gmail.com>
2023-03-14 14:36:47 -0500

    Se agrega el proyecto

---
+++ src/main/java/Conexion/Conexion.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author alejo
+ */
+public class Conexion {
+    private Connection con = null;
+
+    
+    String bd="revision_vehicular_bdd";
+    String url="jdbc:mysql://localhost:3306/";
+   
+    String user = "root";
+    String pass = "Helado.12";
+    String driver="com.mysql.jdbc.Driver";
+    Connection cx;
+    
+    public Conexion(String bd){
+        this.bd=bd;
+    }
+
+  
+    public Connection conecta() throws SQLException {
+        try {
+            Class.forName(driver);
+            cx =  DriverManager.getConnection(url+bd, user, pass);
+            System.out.println("Se conecto a bdd "+bd);
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("No se conecto a la bdd "+bd);
+        }
+        return cx;
+    }
+     
+    public static void main(String[] args) throws SQLException{
+    Conexion conexion= new Conexion("revision_vehicular_bdd");
+    conexion.conecta();
+}
+}
